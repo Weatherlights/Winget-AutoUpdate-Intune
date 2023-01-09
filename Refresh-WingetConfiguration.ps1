175916fb7391eb5c635d3cf3e112ddfbb3050487
Weatherlights <hauke@hauke.us>
2023-01-09 17:19:17 +0100

    tf

---
+++ Refresh-WingetConfiguration.ps1
@@ -0,0 +1,50 @@
+﻿$RegistryLocation = "HKLM:\SOFTWARE\Policies\weatherlights.com\Winget-AutoUpdate";
+$ListLocation = $RegistryLocation + "\List"
+$programdata = "$env:Programdata\Intune-WingetConfigurator";
+$parsedListLocation = "$env:Programdata\Intune-WingetConfigurator\List.txt";
+
+if ( Test-Path -Path $RegistryLocation ) {
+    $configuration = Get-ItemProperty -Path $RegistryLocation;
+}
+
+if ( Test-Path -Path $ListLocation ) {
+    $list = Get-ItemProperty -Path $ListLocation -
+}
+
+function Write-ListConfigToFile {
+    param(
+        $FilePath,
+        $List
+    )
+
+    $parsedList = "";
+
+    ForEach ( $item in $list.PSObject.Properties | where { $_.Name -match "[0-9]+" } )
+    {
+        $parsedList += $item.Value + "`n"
+    }
+
+    Out-File -FilePath $parsedListLocation -InputObject $parsedList
+}
+
+Write-ListConfigToFile -FilePath $ListLocation -List $list;
+
+function Get-CommandLine {
+    param(
+        $configuration
+    )
+
+    $commandLineArgument = "-ListPath $parsedListLocation"
+
+    if ( $configuration.NotificationLevel ) {
+        $commandLineArgument += "-NotificationLevel " + $configuration.NotificationLevel;
+    }
+
+    if ( $configuration.RunOnMetered ) {
+        $commandLineArgument += "-RunOnMetered";
+    }
+
+    if ( $configuration.UseWhiteList ) {
+        $commandLineArgument += "-UseWhiteList";
+    }
+}
