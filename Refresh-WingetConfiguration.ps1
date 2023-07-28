cf0c0c78856c58d70136dd0320998080dfb27bea
Weatherlights <hauke@hauke.us>
2023-07-28 17:04:58 +0200

    Missed a spot

--- Refresh-WingetConfiguration.ps1
+++ Refresh-WingetConfiguration.ps1
@@ -243,7 +243,7 @@ if ( Test-Path -Path $PolicyRegistryLocation ) {
         $commandLineArguments = Get-CommandLine -configuration $configuration;
         Write-LogFile -InputObject "The client MDM or domain joined. Therefore the default enterprise configuration is enabled." -Severity 1
      } else {
-        $commandLineArguments = "-silent -DisableWAUAutoUpdate -NoClean -StartMenuShortcut"
+        $commandLineArguments = "-silent -DoNotUpdate -DisableWAUAutoUpdate -NoClean -StartMenuShortcut"
         Write-LogFile -InputObject "The client is not domain joined or MDM enrolled. Therefore the default enduser configuration is enabled." -Severity 1
      }
 
